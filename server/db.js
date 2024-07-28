d4430dbe0590c49a8280a5c4470b3a969df0aa5e
Haythem91 <haylive8@gmail.com>
2024-07-28 23:52:16 +0100

    VERCEL DB

--- server/db.js
+++ server/db.js
@@ -3,11 +3,9 @@ const Pool = require ('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-    user:process.env.USERNAME,
-    password:process.env.PASSWORD,
-    host:process.env.HOST,
-    port:process.env.DBPORT,
-    database:'todoapp'
-})
+
+    connectionString: process.env.POSTGRES_URL,
+  
+  })
 
 module.exports=pool
